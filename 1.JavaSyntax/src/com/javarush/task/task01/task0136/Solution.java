7a63419a521749337c51681c8b81b9c7348f6107
Ivan <2622610@gmail.com>
2017-11-01 10:53:36 +0300

    решение задачи 214

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0136/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task01.task0136;
+
+/* 
+Да хоть на Луну!
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(getWeight(888));
+    }
+
+    public static double getWeight(int weightEarth) {
+        double ves = (weightEarth * 17 / 100.0);
+        return ves;
+
+       /// double b = (weightEarth * 17 / 100.0);
+        ///return b;
+    }
+}
