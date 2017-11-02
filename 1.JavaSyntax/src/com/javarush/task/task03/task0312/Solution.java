f4ccb6866249123226eda610ac0038196fed2093
Ivan <2622610@gmail.com>
2017-11-03 01:19:41 +0300

    решены задачи 3го уровня

--- 1.JavaSyntax/src/com/javarush/task/task03/task0312/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0312/Solution.java
@@ -8,6 +8,12 @@ public class Solution {
     //напишите тут ваш код
 
     public static void main(String[] args) {
+        System.out.println(convertToSeconds(2));
+        System.out.println(convertToSeconds(4));
         //напишите тут ваш код
+    }
+
+    public static int convertToSeconds(int hour) {
+        return hour * 3600;
     }
 }
