4a05beb17afc1bdeacba06262b4125bed91ffc25
Ivan <2622610@gmail.com>
2017-11-08 01:54:52 +0300

    решены задачи 4го уровня

--- 1.JavaSyntax/src/com/javarush/task/task04/task0417/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0417/Solution.java
@@ -5,9 +5,22 @@ package com.javarush.task.task04.task0417;
 */
 
 import java.io.*;
+import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        Scanner sc = new Scanner(System.in);//напишите тут ваш код
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        if (a==b && b==c)
+            System.out.println(a + " " + b + " " + c);
+        if (a==b && b!=c)
+            System.out.println(a + " " + b);
+        if (b==c && c!=a)
+            System.out.println(b + " " + c);
+        if (c==a && a!=b)
+            System.out.println(c + " " + a);
     }
 }
