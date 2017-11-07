ef03721054ee0e3e16d18b8eb4a012494670474c
Ivan <2622610@gmail.com>
2017-11-07 21:32:15 +0300

    решены задачи 4го уровня

--- 1.JavaSyntax/src/com/javarush/task/task04/task0416/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0416/Solution.java
@@ -5,9 +5,27 @@ package com.javarush.task.task04.task0416;
 */
 
 import java.io.*;
+import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        Scanner sc = new Scanner(System.in);
+        double t = sc.nextInt();
+        float green,red,yellow;  /// 3 мин зеленый 1 мин желтый 1 мин красный
+
+        /// можно реализовать через if else а можно через цикл for - церез деление с остатком на размер цикла
+
+         /* Данная версия через ветвление не работает
+         if (t==4 || t==9 || t==14 || t==19 || t==24 || t==29 || t==34 || t==39 || t==44 || t==49 || t==54 || t==59) {
+             System.out.println("Желтый");
+         } else if (t==5 || t==10 || t==15 || t==20 || t==25 || t==30 || t==35 || t==40 || t==45 || t==50 || t==55) {
+             System.out.println("Красный");
+         } else  {
+             System.out.println("Зеленый");
+        } */
+
+         /// Версия через for
+        for ()
+
     }
 }
