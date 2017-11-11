9bc6f009af4d97bba784faeef4d0ae0a62d94e06
Ivan <2622610@gmail.com>
2017-11-12 00:20:32 +0300

    решены задачи 4го уровня

--- 1.JavaSyntax/src/com/javarush/task/task04/task0424/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0424/Solution.java
@@ -8,6 +8,19 @@ import java.io.*;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String a1 = reader.readLine();
+        String b1 = reader.readLine();
+        String c1 = reader.readLine();
+        int a = Integer.parseInt(a1);
+        int b = Integer.parseInt(b1);
+        int c = Integer.parseInt(c1);
+        if (a != b && b==c)
+            System.out.println(1);
+        if (b !=a && a==c)
+            System.out.println(2);
+        if (c !=b && a==b)
+            System.out.println(3);
+
     }
 }
