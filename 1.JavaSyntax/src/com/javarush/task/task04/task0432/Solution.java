0d00a1991f6c8ccc85e8e908138883f16b9456a4
Ivan <2622610@gmail.com>
2017-11-13 02:53:57 +0300

    решены задачи 4го уровня

--- 1.JavaSyntax/src/com/javarush/task/task04/task0432/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0432/Solution.java
@@ -7,10 +7,20 @@ package com.javarush.task.task04.task0432;
 */
 
 import java.io.*;
+import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        Scanner sc = new Scanner(System.in);
+        Scanner sc1 = new Scanner(System.in);
+
+        String ne = sc1.nextLine();
+        int n = sc.nextInt();
+
+        while (n > 0) {
+            System.out.println(ne);
+            n--;
+        }
 
     }
 }
