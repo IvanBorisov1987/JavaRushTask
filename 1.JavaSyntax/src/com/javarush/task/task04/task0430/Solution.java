29b7903d9c9fa151c5f37b633ae5d1a3afe135f4
Ivan <2622610@gmail.com>
2017-11-12 18:28:40 +0300

    решены задачи 4го уровня

--- 1.JavaSyntax/src/com/javarush/task/task04/task0430/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0430/Solution.java
@@ -8,7 +8,11 @@ import java.io.*;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        int i = 1;
 
+        while (i<11) {
+            System.out.println(i);
+        i++;
+        }
     }
 }
