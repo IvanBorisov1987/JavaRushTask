c557ec2ac5942bee69b8298c710ba97b7e153a6c
Ivan <2622610@gmail.com>
2017-11-12 11:49:44 +0300

    решены задачи 4го уровня

--- 1.JavaSyntax/src/com/javarush/task/task04/task0427/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0427/Solution.java
@@ -1,14 +1,32 @@
 package com.javarush.task.task04.task0427;
 
-/* 
-Описываем числа
-*/
-
-import java.io.*;
+import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
 
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+
+        if (a>=1 && a<=999)
+            if (a%2==0 && a>0 && a<10)
+            System.out.println("четное однозначное число");
+
+        if (a%2!=0 && a>0 && a<10)
+            System.out.println("нечетное однозначное число");
+
+        if (a%2==0 && a>9 && a<100)
+            System.out.println("четное двузначное число");
+
+        if (a%2!=0 && a>9 && a<100)
+            System.out.println("нечетное двузначное число");
+
+        if (a%2==0 && a>99 && a<1000)
+            System.out.println("четное трехзначное число");
+
+        if (a%2!=0 && a>99 && a<1000)
+            System.out.println("нечетное трехзначное число");
+        }
     }
-}
+
