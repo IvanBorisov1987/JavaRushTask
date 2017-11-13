c9c8dc28bd11cb755fc9e641beca4943cc13eca6
Ivan <2622610@gmail.com>
2017-11-13 04:20:54 +0300

    решены задачи 4го уровня

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0438/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task04.task0438;
+
+/* 
+Рисуем линии
+*/
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+
+        String s="";
+        for(int x=0;x<10;x++){
+            s=s+"8";
+        }
+        System.out.print(s);
+        for(int x=0;x<10;x++){
+            s="8";
+            System.out.println(s);
+        }
+    }
+}
+
