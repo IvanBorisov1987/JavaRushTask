e15baef11b2959e00669e4921e74ef7e3bfa9137
Ivan <2622610@gmail.com>
2017-11-14 08:27:45 +0300

    решены задачи 4го уровня

--- 1.JavaSyntax/src/com/javarush/task/task04/task0437/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0437/Solution.java
@@ -7,7 +7,7 @@ package com.javarush.task.task04.task0437;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        
+
         for (int i = 1; i <= 10; i++){
 
             for (int j = 1; j <= i; j++){
