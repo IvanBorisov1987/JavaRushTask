188dbe4a66ba4bd3802ac80b84ea54492d6d4e7d
Ivan <2622610@gmail.com>
2017-11-17 12:49:22 +0300

    решены задачи 5-го уровня Syntax на создание объектов с паратрами.

---
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0504/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task05.task0504;
+
+
+/* 
+Трикотаж
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Cat cat1 = new Cat("Vas",4,16,34);
+        Cat cat2 = new Cat("Boris",2,10,25);
+        Cat cat3 = new Cat("Lev",1,12,30);
+
+    }
+
+    public static class Cat {
+        private String name;
+        private int age;
+        private int weight;
+        private int strength;
+
+        public Cat(String name, int age, int weight, int strength) {
+            this.name = name;
+            this.age = age;
+            this.weight = weight;
+            this.strength = strength;
+        }
+    }
+}
