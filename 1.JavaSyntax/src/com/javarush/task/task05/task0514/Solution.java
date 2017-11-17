2533117dd3b31ea5ffb42790bd0db08556308d91
Ivan <2622610@gmail.com>
2017-11-18 02:06:06 +0300

    #инициализация #объект

--- 1.JavaSyntax/src/com/javarush/task/task05/task0514/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0514/Solution.java
@@ -6,10 +6,17 @@ package com.javarush.task.task05.task0514;
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
+        Person person = new Person();
+        person.initialize("Чел", 23);//напишите тут ваш код
     }
 
     static class Person {
-        
+        String name;
+        int age;
+
+       public void initialize(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
     }
 }
