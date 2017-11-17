95d73f64b8d8c6015c3f15a043c2e1689005c06f
Ivan <2622610@gmail.com>
2017-11-17 11:59:14 +0300

    решены задачи 5-го уровня Syntax на объекты и классы

--- 1.JavaSyntax/src/com/javarush/task/task05/task0502/Cat.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0502/Cat.java
@@ -14,23 +14,7 @@ public class Cat {
     }
 
     public boolean fight(Cat anotherCat) {
-        Cat cat1 = new Cat();
-        Cat cat2 = new Cat();
-        Cat vinner = new Cat();
-
-        cat1.name = "Vasya";
-        cat1.age = 3;
-        cat1.strength = 25;
-        cat1.weight = 18;
-
-        cat2.name = "Barsik";
-        cat2.age = 4;
-        cat2.strength = 30;
-        cat2.weight = 17;
-
-        
-
-        return vinner;
+        //напишите тут ваш код
     }
 
     public static void main(String[] args) {
