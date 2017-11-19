62dcd80eb501ae6231e6fe09a07755e370eb6b42
Ivan <2622610@gmail.com>
2017-11-20 02:26:04 +0300

    #конструктор

---
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0516/Friend.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task05.task0516;
+
+/* 
+Друзей не купишь
+*/
+
+public class Friend {
+    String name;
+    int age;
+    char sex;
+    
+
+    public static void main(String[] args) {
+
+    }
+}
