3de87e0ae58a0f4ff043f9f410b49f42aac5c503
Ivan <2622610@gmail.com>
2017-11-20 03:04:28 +0300

    #объект #конструктор

--- 1.JavaSyntax/src/com/javarush/task/task05/task0516/Friend.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0516/Friend.java
@@ -5,10 +5,25 @@ package com.javarush.task.task05.task0516;
 */
 
 public class Friend {
-    String name;
-    int age;
-    char sex;
-    
+    private String name;
+    private int age;
+    private char sex;
+
+    public Friend(String name) {
+        this.name = name;
+    }
+
+    public Friend(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Friend(String name, int age, char sex) {
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+    }
+
 
     public static void main(String[] args) {
 
