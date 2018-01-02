25aa09681c180f249c9562a7d735213b8998b99a
PillarDevelopment <2622610@gmail.com>
2018-01-02 13:39:51 +0300

    #arrays

--- 1.JavaSyntax/src/com/javarush/task/task07/task0706/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0706/Solution.java
@@ -10,6 +10,26 @@ import java.io.InputStreamReader;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+
+        int even = 0;
+        int uneven = 0;
+        BufferedReader numbers = new BufferedReader(new InputStreamReader(System.in));
+        int[] array = new int[15]; //Создать массив на 15 целых чисел.
+
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = Integer.parseInt(numbers.readLine());
+            if (i == 0)
+                even += array[i];
+            else if (i % 2 == 0)
+                even += array[i];
+            else
+                uneven += array[i];
+        }
+
+        if (even > uneven) {
+            System.out.println("В домах с четными номерами проживает больше жителей.");
+        } else
+            System.out.println("В домах с нечетными номерами проживает больше жителей.");
     }
 }
