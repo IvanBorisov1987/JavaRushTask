74653ca418b3a61ae2dcd6504c25cd29c563f68c
PillarDevelopment <2622610@gmail.com>
2018-01-28 00:36:58 +0300

    #Array, #ArrayList

--- 1.JavaSyntax/src/com/javarush/task/task07/task0712/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0712/Solution.java
@@ -16,24 +16,41 @@ import java.util.ArrayList;
 public class Solution {
     public static void main(String[] args) throws Exception {
 
+
+        /* переменные min и max указывают некую допустимую область значений
+         * для количества символов, которые могут быть в строке
+         * (строк не может быть меньше 0 и вряд ли кто то сможет написать
+         * почти 10 000 000 символов)
+        */
         int max = 0;
         int min = 9999999;
+
         int indexMax = 0;
         int indexMin = 0;
+
+        /* -- Инициализируем список строк в массив -- */
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         ArrayList<String> list = new ArrayList<>();
+
         for (int i = 0; i < 10; i++) {
             list.add(reader.readLine());
         }
 
+
+        /*
+        * В первом цикле for делаем проверку на максимальное значение количества символов
+        *
+        * */
         for (int i = 0; i < 10; i++) {
-            if (list.get(i).length() > max) {
-                String temp;
-                max = list.get(i).length();
-                temp = list.get(i);
-                indexMax = list.indexOf(temp);
+            if (list.get(i).length() > max) { //если длина стркои больше нуля
+                String temp; //временная переменная
+                max = list.get(i).length(); //переменной макс присваивается значение длины строки
+                temp = list.get(i); //временной переменной присваивается строка
+                indexMax = list.indexOf(temp); //индекс равен длине строки
             }
         }
+
+
         for (int i = 0; i < 10; i++) {
             if (min > list.get(i).length()) {
                 String temp;
@@ -42,22 +59,12 @@ public class Solution {
                 indexMin = list.indexOf(temp);
             }
         }
+
+        /* -- Выводим на печать строки --*/
         if (indexMax<indexMin){
             System.out.println(list.get(indexMax));
         }else {
             System.out.println(list.get(indexMin));
         }
-
-    /*
-        int shorter = Integer.MIN_VALUE;
-        int longer = 1;
-
-        if (shorter > longer) {
-            System.out.println(longer); //вывод самой короткой строки на экран
-        } else {
-            System.out.println(shorter); //вывод самой короткой строки на экран
-        }
-            */
-        //напишите тут ваш код
     }
 }
