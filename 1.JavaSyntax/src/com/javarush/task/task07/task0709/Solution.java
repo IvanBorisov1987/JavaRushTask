3fad9a747cf8725d75b928b615704c4a3a57d5ad
PillarDevelopment <2622610@gmail.com>
2018-01-27 09:59:57 +0300

    #Array #ArrayList неправильное решение задачи JavaSyntax 7.09

--- 1.JavaSyntax/src/com/javarush/task/task07/task0709/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0709/Solution.java
@@ -1,29 +1,49 @@
 package com.javarush.task.task07.task0709;
 
+/**
+ * @author Ivan Borisov
+ * @version 0.1
+ *
+ * **/
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 /* 
 Выражаемся покороче
-Выражаемся покороче
-1. Создай список строк.
-2. Считай с клавиатуры 5 строк и добавь в список.
-3. Используя цикл, найди самую короткую строку в списке.
-4. Выведи найденную строку на экран.
-5. Если таких строк несколько, выведи каждую с новой строки.
-
-
-Требования:
-1. Объяви переменную типа список строк и сразу проинициализируй ee.
-2. Программа должна считывать 5 строк с клавиатуры и записывать их в список.
-3. Программа должна выводить самую короткую строку на экран.
-4. Если есть несколько строк с длиной равной минимальной, то нужно вывести каждую из них с новой строки.
-
 */
 
 public class Solution {
     public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<String> list = new ArrayList<>();
+
+
+        for (int i = 0; i < 5; i++) {
+            list.add(reader.readLine());
+        }
+
+
+        /* создаем переменную Integer, которая измеряет длину строки элемента массива */
+
+        int min = list.get(0).length();  /** @param переменная считает размер строки **/
+
+
+        for (int i = 0; i < list.size(); i++) { // итерация цикла #for
+
+            if (list.get(i).length() < min) {
+                System.out.print(min);
+                min = list.get(i).length();
+            }
+        }
+
+        /* Выполняем проверку переменной min если несколько строк одинаково коротки */
+        for (int i =0; i <5; i++) {
+            if (min == list.get(i).length()) {
+                System.out.println(list.get(i));
+            }
+        }
+
         //напишите тут ваш код
     }
 }
