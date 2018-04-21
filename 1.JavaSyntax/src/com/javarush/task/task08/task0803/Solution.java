9faf5c9720ed509b3dd1e49541d95a5def706d57
PillarDevelopment <2622610@gmail.com>
2018-04-21 22:25:33 +0300

    mod

--- 1.JavaSyntax/src/com/javarush/task/task08/task0803/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0803/Solution.java
@@ -1,40 +1,10 @@
 package com.javarush.task.task08.task0803;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Date;
 
-/* 
-Коллекция HashMap из котов
-*/
-
-public class Solution {
-    public static void main(String[] args) throws Exception {
-        String[] cats = new String[]{"васька", "мурка", "дымка", "рыжик", "серый", "снежок", "босс", "борис", "визя", "гарфи"};
-
-        //HashMap<String, Cat> map = addCatsToMap(cats);
-
-        //for (Map.Entry<String, Cat> pair : map.entrySet()) {
-        //    System.out.println(pair.getKey() + " - " + pair.getValue());
-        //}
-    }
-
-
-   // public static HashMap<String, Cat> addCatsToMap(String[] cats) {
-        //напишите тут ваш код
-
-    //}
-
-
-    public static class Cat {
-        String name;
-
-        public Cat(String name) {
-            this.name = name;
-        }
-
-        @Override
-        public String toString() {
-            return name != null ? name.toUpperCase() : null;
-        }
+public  class  Solution {
+    public   static  void main(String[] args) {
+        Date today = new Date();
+        System.out.println("Current date: " + today);
     }
 }
