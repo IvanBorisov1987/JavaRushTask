aeacbe072e6a4ea45e11ce07928c302989a65866
PillarDevelopment <2622610@gmail.com>
2018-05-02 11:08:16 +0300

    #ArrayList

--- 1.JavaSyntax/src/com/javarush/task/task07/task0717/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0717/Solution.java
@@ -4,21 +4,32 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
-/* 
-Удваиваем слова
-*/
-
 public class Solution {
     public static void main(String[] args) throws Exception {
-        // Считать строки с консоли и объявить ArrayList list тут
+        // 1. Объяви переменную типа список строк и сразу проинициализируй ee.
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<String> list = new ArrayList<>();
 
-        //ArrayList<String> result = doubleValues(list);
+        // 2. Считай 10 строк с клавиатуры и добавь их в список.
+        for (int i = 0; i <3; i++) {
+            //String s = reader.readLine();
+            list.add(reader.readLine());
+        }
 
+        ArrayList<String> result = doubleValues(list);
         // Вывести на экран result
+        for (int i = 0; i < result.size(); i++) {
+            System.out.println( result.get(i));
+        }
     }
-
+    // 3. Метод doubleValues должен удваивать элементы списка по принципу a,b,c -> a,a,b,b,c,c..
     public static ArrayList<String> doubleValues(ArrayList<String> list) {
-        //напишите тут ваш код
-        return null;
+
+        int size = list.size();
+            for (int i = 0; i < size; i++) {
+                list.add(list.get(i));
+                list.add(list.get(i));
+            }
+        return list;
     }
 }
