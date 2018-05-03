62708665df8e28f5221a669b30359f3aa25daaba
PillarDevelopment <2622610@gmail.com>
2018-05-04 02:38:14 +0300

    задача на перестановку элементов списка Collections.rotate

--- 1.JavaSyntax/src/com/javarush/task/task07/task0720/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0720/Solution.java
@@ -9,27 +9,24 @@ import java.util.Collections;
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        //1. Объяви переменную типа список строк и сразу проинициализируй ee.
         ArrayList<String> list = new ArrayList<String>() ;
 
-        int N; // Количество строк в массиве
-        int M; // количество первых строк для переноса в конец
-        //Ввести с клавиатуры 2 числа N и M.
+        int N;
+        int M;
+
         String s1 = reader.readLine();
         N = Integer.parseInt(s1);
         String s2 = reader.readLine();
         M = Integer.parseInt(s2);
 
-        // Ввести N строк и заполнить ими список.
         for (int i = 0; i < N; i++) {
             String str = reader.readLine();
             if (str.isEmpty()) break;
             list.add(str);
         }
-        // 3. Переставить M первых строк в конец списка.
-        Collections.rotate(list, M);
 
-        // 4. Выведи список на экран, каждое значение с новой строки
+        Collections.rotate(list, list.size() - M);
+
         for (int i = 0; i < N; i++) {
             System.out.println(list.get(i));
         }
