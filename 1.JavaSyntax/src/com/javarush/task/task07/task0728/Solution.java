9ef37ad72a2958157ad6e6c05058b12d0a4d7a2b
PillarDevelopment <2622610@gmail.com>
2018-05-04 02:21:48 +0300

    mod

--- 1.JavaSyntax/src/com/javarush/task/task07/task0728/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0728/Solution.java
@@ -11,7 +11,6 @@ import java.util.Arrays;
 public class Solution {
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
         int[] array = new int[20];
         for (int i = 0; i < 20; i++) {
             array[i] = Integer.parseInt(reader.readLine());
