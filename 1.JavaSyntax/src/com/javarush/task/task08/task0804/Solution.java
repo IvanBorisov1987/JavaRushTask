693efd6821510d37c3334dc2c931aa8eaf813c32
PillarDevelopment <2622610@gmail.com>
2018-05-07 15:18:39 +0300

    Задача на печать коллекций HashMap

--- 1.JavaSyntax/src/com/javarush/task/task08/task0804/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0804/Solution.java
@@ -3,13 +3,11 @@ package com.javarush.task.task08.task0804;
 import java.util.HashMap;
 import java.util.Map;
 
-/* 
-Вывести на экран список ключей
-*/
-
 public class Solution {
     public static void main(String[] args) throws Exception {
+
         HashMap<String, String> map = new HashMap<String, String>();
+
         map.put("Sim", "Sim");
         map.put("Tom", "Tom");
         map.put("Arbus", "Arbus");
@@ -25,6 +23,11 @@ public class Solution {
     }
 
     public static void printKeys(Map<String, String> map) {
-        //напишите тут ваш код
+
+        for (Map.Entry<String, String> pair : map.entrySet())
+        {
+            String key = pair.getKey();                      //ключ
+            System.out.println(key);
+        }
     }
 }
