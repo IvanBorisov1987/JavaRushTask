390938c92f21739bd62900a849daacf2c8eee0cb
PillarDevelopment <2622610@gmail.com>
2018-05-11 09:25:21 +0300

    задача на get, set, remove, add для коллекций

--- 1.JavaSyntax/src/com/javarush/task/task08/task0807/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0807/Solution.java
@@ -2,22 +2,19 @@ package com.javarush.task.task08.task0807;
 
 import java.util.*;
 
-/* 
-LinkedList и ArrayList
-*/
-
 public class Solution {
     public static Object createArrayList() {
-        //напишите тут ваш код
-
+        ArrayList<String> list = new ArrayList<>();
+        return list;
     }
 
     public static Object createLinkedList() {
-        //напишите тут ваш код
-
+        LinkedList<String> list = new LinkedList<>();
+        return list;
     }
 
     public static void main(String[] args) {
-
+        createArrayList();
+        createLinkedList();
     }
 }
