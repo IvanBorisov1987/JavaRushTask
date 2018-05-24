171793017332fe56ca91f60a55f0b8e4e65e0227
PillarDevelopment <2622610@gmail.com>
2018-05-25 01:48:34 +0300

    Дата Коллекции HashMap

--- 1.JavaSyntax/src/com/javarush/task/task08/task0816/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0816/Solution.java
@@ -1,37 +1,37 @@
 package com.javarush.task.task08.task0816;
 
+import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.HashMap;
+import java.util.*;
 
 public class Solution {
-    public static HashMap<String, Date> createMap() {
+    public static HashMap<String, Date> createMap() throws ParseException {
 
-        SimpleDateFormat
-        HashMap<String, Date> map = new HashMap<String, Date>();
+        DateFormat df = new SimpleDateFormat("MMMMM d yyyy", Locale.ENGLISH); // английский формат даты
+        HashMap<String, Date> map = new HashMap<>();
 
-        map.put("Stallone", new Date("JUNE 1 1980")); // 1
-        map.put("ShvarzNiger", new Date("JULE 2 1981")); // 2
-        map.put("DeNiro", new Date("Mart 4 1975")); // 3
-        map.put("Roberts", new Date("January 21 1970")); // 4
-        map.put("AlPachino", new Date("April 7, 1960")); // 5
-        map.put("Norris", new Date("February 31 1001")); // 6
-        map.put("Li", new Date("May 3 1963")); // 7
-        map.put("Chan", new Date("AUGUST 8 1973")); // 8
-        map.put("Sigal", new Date("AUGUST 22 1945")); // 9
-        map.put("Girr", new Date("February 8 1962")); // 10
+        // циклом добавляются фамилии и годы рождения
+        for (int i = 1; i <= 10; i++) {
+            map.put("Stallone" + i, df.parse("JUNE " + i + " 1980"));
+        }
 
         return map;
-        //напишите тут ваш код
     }
 
     public static void removeAllSummerPeople(HashMap<String, Date> map) {
-        for (int i = 0; i < map.size(); i++)
-            if (map.values(i) == "JUNE" || map.values(i) == "JULE" || map.values(i) == "AUGUST") {
-            map.remove(i,i);
-            } else
-                break;
+        Collection<Integer> removeCandidates = new LinkedList<>();
 
+        Calendar myCal = new GregorianCalendar();
+
+        for (Iterator<HashMap.Entry<String, Date>> it = map.entrySet().iterator(); it.hasNext(); ) {
+            HashMap.Entry<String, Date> entry = it.next();
+            myCal.setTime(entry.getValue());
+            int a = myCal.get(Calendar.MONTH) + 1;
+            if (a >= 6 && a <= 8) {
+                it.remove();
+            }
+        }
     }
 
     public static void main(String[] args) {
@@ -39,5 +39,19 @@ public class Solution {
     }
 }
 
-/* Добрая Зинаида и летние каникулы
-4. Метод removeAllSummerPeople() должен удалять из словаря всех людей, родившихся летом.*/
+/*
+
+
+        //напишите тут ваш код
+    }
+
+    public static void removeAllSummerPeople(HashMap<String, Date> map) {
+        //напишите тут ваш код
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+* */
